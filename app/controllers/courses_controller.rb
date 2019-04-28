9e666b5b720aee2c89859a0100ccd89c48ddaf6f
gage.nevins@gmail.com <gage.nevins@gmail.com>
2019-04-28 20:46:04 +0000

    Carrierwave

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,10 @@
 class CoursesController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+        @course = Course.new
+    end
+
     def index
         @courses = Course.all
     end
