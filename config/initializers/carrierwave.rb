67a40ceef709d331b6335ca2a45ba4b310db0fc6
gage.nevins@gmail.com <gage.nevins@gmail.com>
2019-05-04 17:48:20 +0000

    image issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,21 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl    = "public-read"
+  :s3_permissions => 'public-read'
 
   config.aws_credentials = {
       access_key_id:     ENV["AWS_ACCESS_KEY"],
       secret_access_key: ENV["AWS_SECRET_KEY"],
       region:            ENV["AWS_REGION"]
+  }
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET_ID'],
+      :access_key_id => ENV['AWS_ACCESS_KEY'],
+      :secret_access_key => ENV['AWS_SECRET_KEY']
+    },
+    :s3_region => ENV['AWS_REGION']
   }
   end
