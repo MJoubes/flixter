3ee580698179147dd707a64f6a9dc449b10e8091
gage.nevins@gmail.com <gage.nevins@gmail.com>
2019-05-17 21:33:35 +0000

    Video uploading fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,4 +12,9 @@ CarrierWave.configure do |config|
        region:            ENV["AWS_REGION"] # required
    }
              # required
+
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
  end
